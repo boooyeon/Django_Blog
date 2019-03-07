95cb8bbc2dab2fdf0049f8d6e05b4f098a242410
boooyeon <boooyeon@gmail.com>
2019-03-07 14:44:58 +0900

    feat: Init project

---
+++ blogapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('', views.blog, name='blog'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+ 
+    path('create', views.create, name='create'),
+    path('newblog/', views.blogpost, name='newblog')
+
+  
+]
